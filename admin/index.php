64cc35a57d62b9ad757a676e6cb3e56462e02ba6
Alan Nato <alanwnato@gmail.com>
2023-12-21 19:39:48 +0300

    feat : added features

--- admin/index.php
+++ admin/index.php
@@ -12,36 +12,19 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. -->
 <!DOCTYPE html>
 <html lang="en" itemscope itemtype="http://schema.org/WebPage">
-
 <head>
-
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="apple-touch-icon" sizes="76x76" href="img/logo.png">
+<link rel="icon" type="image/png" href="img/logo.png">
 
-<link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon.png">
-<link rel="icon" type="image/png" href="img/favicon.png">
+<title> Utumishi | LOGIN </title>
 
-<title> Utumishi </title>
-
-
-
-<!--     Fonts and icons     -->
 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
-
-<!-- Nucleo Icons -->
 <link href="css/nucleo-icons.css" rel="stylesheet" />
 <link href="css/nucleo-svg.css" rel="stylesheet" />
-
-<!-- Font Awesome Icons -->
 <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-
-<!-- Material Icons -->
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
-
-<!-- CSS Files -->
-
-
-
 <link id="pagestyle" href="css/material-kit.css?v=3.0.4" rel="stylesheet" />
 
 </head>
@@ -54,24 +37,30 @@
       <div class="row">
         <div class="col-lg-4 col-md-8 col-12 mx-auto">
           <div class="card z-index-0 fadeIn3 fadeInBottom">
-            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-              <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
-                <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Sign in</h4>
-              </div>
-            </div>
+
+        <div class="col-lg-6 mt-lg-0 mt-5 ps-lg-0 ps-0">
+          <div class="p-3 info-horizontal">
+          <center><img src="img/logo.png" height="100px" width="100px"></center>
+          </div>
+        <div class="description ps-3">
+        <h4 class="text-Secondary font-weight-bolder text-center mt-2 mb-0">Utumishi</h4>
+        <h5 class="text-Secondary font-weight-bolder text-center mt-2 mb-0">Here To Serve</h5>
+          </div>
+        </div>
+          
             <div class="card-body">
-              <form role="form" class="text-start">
+              <form role="form" class="text-start" action="" method="post">
                 <div class="input-group input-group-outline my-3">
                   <label class="form-label">Email</label>
-                  <input type="email" class="form-control">
+                  <input type="email" name="email" class="form-control">
                 </div>
                 <div class="input-group input-group-outline mb-3">
                   <label class="form-label">Password</label>
-                  <input type="password" class="form-control">
+                  <input type="password" name="password" class="form-control">
                 </div>
 
                 <div class="text-center">
-                  <button type="button" class="btn bg-gradient-primary w-100 my-4 mb-2">Sign in</button>
+                  <button type="submit" name="submit" class="btn bg-gradient-info w-100 my-4 mb-2">Sign in</button>
                 </div>
 
               </form>
@@ -80,113 +69,43 @@
         </div>
       </div>
     </div>
-    
+  
+<?php
+  if(isset($_POST["submit"])){
+    if(!empty($_POST['email']) && !empty($_POST['password'])){
+      $email = $_POST['email'];
+      $password = $_POST['password'];
+      $conn = new mysqli('localhost', 'root', '') or die(mysqli_error());
+      $db = mysqli_select_db($conn, 'utumishi') or die("DB Error");
+      $query = mysqli_query($conn, "SELECT * FROM staff WHERE email='".$email."' and password='".$password."'");
+      $numrows = mysqli_num_rows($query);
+        if($numrows !=0){
+          while($row = mysqli_fetch_assoc($query)){
+          $dbusername = $row['email'];
+          $dbpassword = $row['password'];
+          }
+        if($email == $dbusername && $password == $dbpassword){
+          session_start();
+          $_SESSION['sess_user']=$email;
+          header("Location: home.php");
+          }
+          }
+        else{
+         echo "<span class='alert alert-danger text-center mt-10'>Incorrect Login Details</span>";
+        }
+        }
+        else
+        {
+          echo "<span class='alert alert-danger text-center mt-10'>All fields required</span>";
+        }
+  }
+?>
 
-<!--   Core JS Files   -->
 <script src="js/core/popper.min.js" type="text/javascript"></script>
 <script src="js/core/bootstrap.min.js" type="text/javascript"></script>
 <script src="js/plugins/perfect-scrollbar.min.js"></script>
-
-
-
-
-<!--  Plugin for TypedJS, full documentation here: https://github.com/inorganik/CountUp.js -->
-<script src="js/plugins/countup.min.js"></script>
-
-
-
-
-
-<script src="js/plugins/choices.min.js"></script>
-
-
-
-<script src="js/plugins/prism.min.js"></script>
-<script src="js/plugins/highlight.min.js"></script>
-
-
-
-<!--  Plugin for Parallax, full documentation here: https://github.com/dixonandmoe/rellax -->
 <script src="js/plugins/rellax.min.js"></script>
-<!--  Plugin for TiltJS, full documentation here: https://gijsroge.github.io/tilt.js/ -->
-<script src="js/plugins/tilt.min.js"></script>
-<!--  Plugin for Selectpicker - ChoicesJS, full documentation here: https://github.com/jshjohnson/Choices -->
-<script src="js/plugins/choices.min.js"></script>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- Control Center for Material UI Kit: parallax effects, scripts for the example pages etc -->
-<!--  Google Maps Plugin    -->
-
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTTfWur0PDbZWPr7Pmq8K3jiDp0_xUziI"></script>
 <script src="js/material-kit.min.js?v=3.0.4" type="text/javascript"></script>
-
-
-<script type="text/javascript">
-
-  if (document.getElementById('state1')) {
-    const countUp = new CountUp('state1', document.getElementById("state1").getAttribute("countTo"));
-    if (!countUp.error) {
-      countUp.start();
-    } else {
-      console.error(countUp.error);
-    }
-  }
-  if (document.getElementById('state2')) {
-    const countUp1 = new CountUp('state2', document.getElementById("state2").getAttribute("countTo"));
-    if (!countUp1.error) {
-      countUp1.start();
-    } else {
-      console.error(countUp1.error);
-    }
-  }
-  if (document.getElementById('state3')) {
-    const countUp2 = new CountUp('state3', document.getElementById("state3").getAttribute("countTo"));
-    if (!countUp2.error) {
-      countUp2.start();
-    } else {
-      console.error(countUp2.error);
-    };
-  }
-</script>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 </body>
 
