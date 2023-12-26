163383f7fe0bf809ec3e66a2c9ac78503be73f5a
Alan Nato <alanwnato@gmail.com>
2023-12-26 22:38:16 +0300

    fix : minor fixes

--- app/dashboard/index.php
+++ app/dashboard/index.php
@@ -18,20 +18,16 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="apple-touch-icon" sizes="76x76" href="../img/logo.png">
-<link rel="icon" type="image/png" href="../img/logo.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="../images/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="../images/logo.png">
 
-<title> Utumishi | LOGIN </title>
+<title> utumishi | LOGIN </title>
 
-  <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-  <!-- Nucleo Icons -->
   <link href="css/nucleo-icons.css" rel="stylesheet" />
   <link href="css/nucleo-svg.css" rel="stylesheet" />
-  <!-- Font Awesome Icons -->
   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
   <link href="css/nucleo-svg.css" rel="stylesheet" />
-  <!-- CSS Files -->
   <link id="pagestyle" href="css/argon-dashboard.css?v=2.0.4" rel="stylesheet" />
 </head>
 
@@ -42,52 +38,27 @@
         <div class="container">
           <div class="row">
             <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
-            <center><img src="../img/logo.png" height="100px" width="100px"></center>
+            <center><img src="../images/logo.png" height="300px" width="300px"></center>
               <div class="card card-plain">
-              <h4 class="text-Secondary font-weight-bolder text-center mt-2 mb-0">Utumishi</h4>
-        <h5 class="text-Secondary font-weight-bolder text-center mt-2 mb-0">Here To Serve</h5>
-                <div class="card-header pb-0 text-start">
-                  <h4 class="font-weight-bolder">Sign In</h4>
-                  <p class="mb-0">Enter your email and password to sign in</p>
+                <h4 class="text-Secondary font-weight-bolder text-center mt-2 mb-0">utumishi | Dashboard</h4>
+                <br>
+                <h5 class="text-Secondary font-weight-bolder text-center mt-2 mb-0">Empowering law enforcement with reliable technology.</h5>
+                <br>
+                <div class="card-header pb-0 text-center">
+                <h4 class="font-weight-bolder">Sign In</h4>
+                <p class="mb-0">Enter your email and password to sign in.</p>
                 </div>
+
                 <div class="card-body">
-                  <form role="form" action="" method="post">
-                    <div class="mb-3">
-                      <input type="email" name="email" class="form-control form-control-lg" placeholder="Email" aria-label="Email">
-                    </div>
-                    <div class="mb-3">
-                      <input type="password" name="password" class="form-control form-control-lg" placeholder="Password" aria-label="Password">
-                    </div>
 
-                    <div class="text-center">
-                      <button type="submit" name="submit" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Sign in</button>
-                    </div>
-                  </form>
-                </div>
-
-              </div>
-            </div>
-            <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
-              <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('../img/facebook_cover_photo_2.png');
-          background-size: cover;  background-repeat: no-repeat;">
-                <span class="mask bg-gradient-primary opacity-6"></span>
-                <h4 class="mt-5 text-white font-weight-bolder position-relative">"Here to Serve"</h4>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </main>
-
-  <?php
+<?php
   if(isset($_POST["submit"])){
     if(!empty($_POST['email']) && !empty($_POST['password'])){
       $email = $_POST['email'];
       $password = $_POST['password'];
       $conn = new mysqli('localhost', 'root', '') or die(mysqli_error());
       $db = mysqli_select_db($conn, 'utumishi') or die("DB Error");
-      $query = mysqli_query($conn, "SELECT * FROM admins WHERE email='".$email."' and password='".$password."'");
+      $query = mysqli_query($conn, "SELECT * FROM staff WHERE email='".$email."' and password='".$password."'");
       $numrows = mysqli_num_rows($query);
         if($numrows !=0){
           while($row = mysqli_fetch_assoc($query)){
@@ -97,19 +68,51 @@
         if($email == $dbusername && $password == $dbpassword){
           session_start();
           $_SESSION['sess_user']=$email;
-          header("Location: home.php");
+          header("Location: home");
           }
           }
         else{
-         echo "<span class='alert alert-danger text-center mt-10'>Incorrect Login Details</span>";
+         echo "<span class='btn btn-danger text-center'>Incorrect Login Details</span>";
         }
         }
         else
         {
-          echo "<span class='alert alert-danger text-center mt-10'>All fields required</span>";
+          echo "<span class='btn btn-danger text-center'>All fields required</span>";
         }
   }
 ?>
+
+              <form role="form" action="" method="post">
+                <div class="input-group input-group-outline my-3">
+                  <input type="email" name="email" class="form-control form-control-lg  input-group-outline" placeholder="Email" aria-label="Email">
+                </div>
+
+                <div class="input-group input-group-outline my-3">
+                  <input type="password" name="password" class="form-control form-control-lg  input-group-outline" placeholder="Password" aria-label="Password">
+                </div>
+
+                <div class="text-center">
+                  <button type="submit" name="submit" class="btn btn-lg btn-info btn-lg w-100 mt-4 mb-0">Sign in</button>
+                </div>
+              </form>
+
+              </div>
+                
+              </div>
+            </div>
+            
+<div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
+  <div class="position-relative bg-gradient-info h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden"
+    style="background-image: url('../images/wallpaper3.jpg');
+    background-size: cover;">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
   <!--   Core JS Files   -->
   <script src="js/core/popper.min.js"></script>
   <script src="js/core/bootstrap.min.js"></script>
