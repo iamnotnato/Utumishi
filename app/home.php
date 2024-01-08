b5385b8ce006010216c0c23b5e983e541d408cbd
Alan Nato <alanwnato@gmail.com>
2024-01-09 01:40:36 +0300

    fix : minor changes

--- app/home.php
+++ app/home.php
@@ -1,134 +1,314 @@
-<?php
-session_start();
-$connect = mysqli_connect("localhost", "root", "", "utumishi");
-if(!isset($_SESSION['sess_user'])) {
-    header("Location:index.php");
-} else {
-    ?>
-
 <!--
 =========================================================
-* Material Kit 2 - v3.0.4
+* Argon Dashboard 2 - v2.0.4
 =========================================================
 
-* Product Page:  https://www.creative-tim.com/product/material-kit
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Coded by www.creative-tim.com
+* Product Page: https://www.creative-tim.com/product/argon-dashboard
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://www.creative-tim.com/license)
+* Coded by Creative Tim
 
- =========================================================
+=========================================================
 
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. -->
-
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
 <!DOCTYPE html>
-<html lang="en" itemscope itemtype="http://schema.org/WebPage">
+<html lang="en">
+
 <head>
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="apple-touch-icon" sizes="76x76" href="img/logo.png">
-<link rel="icon" type="image/png" href="img/logo.png">
-
-<title> utumishi | Home </title>
-
-<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
-<link href="css/nucleo-icons.css" rel="stylesheet" />
-<link href="css/nucleo-svg.css" rel="stylesheet" />
-<script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
-<link id="pagestyle" href="css/material-kit.css?v=3.0.4" rel="stylesheet" />
-
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="../img/logo.png">
+  <link rel="icon" type="image/png" href="../img/logo.png">
+  <title>
+    Argon Dashboard 2 by Creative Tim
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="dashboard/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="dashboard/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="dashboard/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="dashboard/css/argon-dashboard.min.css?v=2.0.4" rel="stylesheet" />
 </head>
 
-<body class="index-page bg-gray-200">
-
-
-<nav class="navbar navbar-expand-lg top-0 z-index-3 w-100 shadow-none my-3  navbar-transparent ">
-  <div class="container">
-    <a class="navbar-brand text-Gray 900" href="" rel="tooltip" title="Empowering law enforcement with reliable technology." data-placement="bottom" target="_blank">
-      <h5>utumishi</h5>
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navigation">
-      <ul class="navbar-nav navbar-nav-hover mx-auto">
-
-        <li class="nav-item px-3">
-          <a class="nav-link text-muted">
-          <span class="d-sm-inline d-none"><h4>Signed in as : <?=$_SESSION['sess_user'];?></h4></span>
+<body class="g-sidenav-show   bg-gray-100">
+  <div class="min-height-300 bg-primary position-absolute w-100"></div>
+  <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
+    <div class="sidenav-header">
+      <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+      <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html " target="_blank">
+        <img src="../images/logo.png" class="navbar-brand-img h-100" alt="main_logo">
+        <span class="ms-1 font-weight-bold">Utumishi</span>
+      </a>
+    </div>
+    <hr class="horizontal dark mt-0">
+    <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="../pages/dashboard.html">
+            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
+            </div>
+            <span class="nav-link-text ms-1">Dashboard</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="cases.php">
+            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+            
+            <i class="fa-solid fa-handcuffs"></i>  
+            <i class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i>
+            </div>
+            <span class="nav-link-text ms-1">Cases</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="../pages/billing.html">
+            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+            <i class="fa-solid fa-car"></i>  
+            <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
+            </div>
+            <span class="nav-link-text ms-1">Lost Cars</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="../pages/virtual-reality.html">
+            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+            <i class="fa-solid fa-building-shield"></i>  
+            <i class="ni ni-app text-info text-sm opacity-10"></i>
+            </div>
+            <span class="nav-link-text ms-1">Evidence Collection</span>
           </a>
         </li>
       </ul>
+    </div>
 
-      <ul class="navbar-nav  justify-content-end">
+  </aside>
+  <main class="main-content position-relative border-radius-lg ">
+    <!-- Navbar -->
+    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur" data-scroll="false">
+      <div class="container-fluid py-1 px-3">
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+            <li class="breadcrumb-item text-sm text-white active" aria-current="page">Dashboard</li>
+          </ol>
+          <h6 class="font-weight-bolder text-white mb-0">Dashboard</h6>
+        </nav>
+        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+          <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+            <div class="input-group">
+
+            </div>
+          </div>
+          <ul class="navbar-nav  justify-content-end">
             <li class="nav-item d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-muted font-weight-bold px-0">
-
-                <span class="d-sm-inline d-none"><h4><a href="logout.php"><i class="fa fa-user fa-2xl text-muted"></i> Sign Out</a></h4></span>
+              <a href="logout.php" class="nav-link text-white font-weight-bold px-0">
+                <i class="fa fa-user me-sm-1"></i>
+                <span class="d-sm-inline d-none">Sign Out</span>
+              </a>
+            </li>
+            <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+              <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
+                <div class="sidenav-toggler-inner">
+                  <i class="sidenav-toggler-line bg-white"></i>
+                  <i class="sidenav-toggler-line bg-white"></i>
+                  <i class="sidenav-toggler-line bg-white"></i>
+                </div>
               </a>
             </li>
 
           </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- End Navbar -->
+    <div class="container-fluid py-4">
 
 
+      <div class="row mt-4">
+        <div class="col-lg-7 mb-lg-0 mb-4">
+          <div class="card ">
+            <div class="card-header pb-0 p-3">
+              <div class="d-flex justify-content-between">
+                <h6 class="mb-2">Sales by Country</h6>
+              </div>
+            </div>
+            <div class="table-responsive">
+              <table class="table align-items-center ">
+                <tbody>
+                  <tr>
+                    <td class="w-30">
+                      <div class="d-flex px-2 py-1 align-items-center">
+                        <div>
+                          <img src="img/icons/flags/US.png" alt="Country flag">
+                        </div>
+                        <div class="ms-4">
+                          <p class="text-xs font-weight-bold mb-0">Country:</p>
+                          <h6 class="text-sm mb-0">United States</h6>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Sales:</p>
+                        <h6 class="text-sm mb-0">2500</h6>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Value:</p>
+                        <h6 class="text-sm mb-0">$230,900</h6>
+                      </div>
+                    </td>
+                    <td class="align-middle text-sm">
+                      <div class="col text-center">
+                        <p class="text-xs font-weight-bold mb-0">Bounce:</p>
+                        <h6 class="text-sm mb-0">29.9%</h6>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="w-30">
+                      <div class="d-flex px-2 py-1 align-items-center">
+                        <div>
+                          <img src="img/icons/flags/DE.png" alt="Country flag">
+                        </div>
+                        <div class="ms-4">
+                          <p class="text-xs font-weight-bold mb-0">Country:</p>
+                          <h6 class="text-sm mb-0">Germany</h6>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Sales:</p>
+                        <h6 class="text-sm mb-0">3.900</h6>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Value:</p>
+                        <h6 class="text-sm mb-0">$440,000</h6>
+                      </div>
+                    </td>
+                    <td class="align-middle text-sm">
+                      <div class="col text-center">
+                        <p class="text-xs font-weight-bold mb-0">Bounce:</p>
+                        <h6 class="text-sm mb-0">40.22%</h6>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="w-30">
+                      <div class="d-flex px-2 py-1 align-items-center">
+                        <div>
+                          <img src="img/icons/flags/GB.png" alt="Country flag">
+                        </div>
+                        <div class="ms-4">
+                          <p class="text-xs font-weight-bold mb-0">Country:</p>
+                          <h6 class="text-sm mb-0">Great Britain</h6>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Sales:</p>
+                        <h6 class="text-sm mb-0">1.400</h6>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Value:</p>
+                        <h6 class="text-sm mb-0">$190,700</h6>
+                      </div>
+                    </td>
+                    <td class="align-middle text-sm">
+                      <div class="col text-center">
+                        <p class="text-xs font-weight-bold mb-0">Bounce:</p>
+                        <h6 class="text-sm mb-0">23.44%</h6>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="w-30">
+                      <div class="d-flex px-2 py-1 align-items-center">
+                        <div>
+                          <img src="img/icons/flags/BR.png" alt="Country flag">
+                        </div>
+                        <div class="ms-4">
+                          <p class="text-xs font-weight-bold mb-0">Country:</p>
+                          <h6 class="text-sm mb-0">Brasil</h6>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Sales:</p>
+                        <h6 class="text-sm mb-0">562</h6>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Value:</p>
+                        <h6 class="text-sm mb-0">$143,960</h6>
+                      </div>
+                    </td>
+                    <td class="align-middle text-sm">
+                      <div class="col text-center">
+                        <p class="text-xs font-weight-bold mb-0">Bounce:</p>
+                        <h6 class="text-sm mb-0">32.14%</h6>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <footer class="footer pt-3  ">
+        <div class="container-fluid">
+          <div class="row align-items-center justify-content-lg-between">
+            <div class="col-lg-6 mb-lg-0 mb-4">
+              <div class="copyright text-center text-sm text-muted text-lg-start">
+                © <script>
+                  document.write(new Date().getFullYear())
+                </script>, Utumishi
+
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </footer>
     </div>
-  </div>
-</nav>
+  </main>
 
-</header>
-<!-- End Navbar -->
-</div></div></div>
+  <!--   Core JS Files   -->
+  <script src="dashboard/js/core/popper.min.js"></script>
+  <script src="dashboard/js/core/bootstrap.min.js"></script>
+  <script src="dashboard/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="dashboard/js/plugins/smooth-scrollbar.min.js"></script>
+  <script src="dashboard/js/plugins/chartjs.min.js"></script>
 
-<header class="header-2">
-  <div class="page-header min-vh-90 relative" style="background-image: url('images/wallpaper.jpg')">
-    <span class="mask bg-gradient-primary opacity-4"></span>
-    <div class="container">
-  <div class="row">
-
-     <div class="col-sm">
-    <span><h4>Current date and time : </h4></span>
-    <span id="datetime"></span>
-
-
-    </div>
-
-    <div class="col-sm">
-    <center><img src="images/wallpaper.jpg" height="600px" width="600px"></center>
-
-              <h4 class="text-Secondary font-weight-bolder text-center mt-2 mb-0">Utumishi</h4>
-        <h5 class="text-Secondary font-weight-bolder text-center mt-2 mb-0">Here To Serve</h5>
-    </div>
-
-
-  <div class="col-sm">
-  <a href="newcases.php"> <button class="btn btn-icon btn-3 btn-primary" type="button">
-	<span class="btn-inner--icon"><i class="fa fa-exclamation me-sm-1"></i></span>
-  <span class="btn-inner--text"> Report Case</span>
-  </button> </a>
-  </div>
-  </div>
-</div>
-  </div>
-</header>
+  <script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="js/argon-dashboard.min.js?v=2.0.4"></script>
 </body>
 
-
-<script>
-      // create a function to update the date and time
-      function updateDateTime() {
-        // create a new `Date` object
-        const now = new Date();
-
-        // get the current date and time as a string
-        const currentDateTime = now.toLocaleString();
-
-        // update the `textContent` property of the `span` element with the `id` of `datetime`
-        document.querySelector('#datetime').textContent = currentDateTime;
-      }
-
-      // call the `updateDateTime` function every second
-      setInterval(updateDateTime, 1000);
-    </script>
 </html>
-<?php
-}
-?>
--- index.php
+++ index.php
@@ -821,4 +821,14 @@
     </div>
   </footer>
 
+  <!--   Core JS Files   -->
+  <script src="app/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="app/js/core/bootstrap.min.js" type="text/javascript"></script>
+  <script src="app/js/plugins/perfect-scrollbar.min.js"></script>
+  <!-- Control Center for Material UI Kit: parallax effects, scripts for the example pages etc -->
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTTfWur0PDbZWPr7Pmq8K3jiDp0_xUziI"></script>
+  <script src="app/js/material-kit.min.js?v=3.0.4" type="text/javascript"></script>
+</body>
+
 </html>
