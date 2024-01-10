9fbad57f77981580abaf7e2bc311e8dbd274b2d1
Alan Nato <alanwnato@gmail.com>
2024-01-10 03:25:33 +0300

    fix : minor fixes

--- app/dashboard/home.php
+++ app/dashboard/home.php
@@ -18,8 +18,8 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="apple-touch-icon" sizes="76x76" href="../img/logo.png">
-  <link rel="icon" type="image/png" href="../img/logo.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="../images/logo.png">
+  <link rel="icon" type="image/png" href="../images/logo.png">
   <title>
    utumishi | Dashboard
   </title>
@@ -28,18 +28,23 @@
 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
 <link href="../css/nucleo-icons.css" rel="stylesheet" />
 <link href="../css/nucleo-svg.css" rel="stylesheet" />
-<script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/42d5adcbca.../js" crossorigin="anonymous"></script>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
 <link id="pagestyle" href="../css/material-kit.min.css?v=3.0.4" rel="stylesheet" />
 </head>
 
 <body class="g-sidenav-show bg-dark">
-  <div class="min-height-300 bg-dark position-absolute w-100"></div>
-  <aside class="sidenav bg-secondary navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
-    <div class="sidenav-header">
+  <div class="min-height-300 bg-dark position-absolute w-100"  
+  style="background-image: url('../images/wallpaper.jpg'); background-repeat: no-repeat; background-size: cover; background-blend-mode: lighten;">
+    <div class="sidenav-header" ></div>
+
+  
+  <aside class="sidenav bg-secondary navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main" 
+  style="background-image: url('../images/bg9.jpg'); background-repeat: no-repeat; background-size: cover; background-blend-mode: lighten;">
+    <div class="sidenav-header" >
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href="" target="_blank">
-        <img src="../images/logo.png" class="navbar-brand-img h-100" alt="main_logo">
+        <images src="../images/logo.png" class="navbar-brand-images h-100" alt="main_logo">
         <span class="ms-1 font-weight-bold">Utumishi</span>
         <br>
         <br>
@@ -72,7 +77,7 @@
     <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" href="../pages/dashboard.html">
+          <a class="nav-link active" href="pages/dashboard.html">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
             </div>
@@ -90,7 +95,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link " href="../pages/billing.html">
+          <a class="nav-link " href="pages/billing.html">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
             <i class="fa-solid fa-car"></i>  
             <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
@@ -99,7 +104,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link " href="../pages/virtual-reality.html">
+          <a class="nav-link " href="pages/virtual-reality.html">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
             <i class="fa-solid fa-building-shield"></i>  
             <i class="ni ni-app text-info text-sm opacity-10"></i>
@@ -110,7 +115,7 @@
 
 
         <li class="nav-item">
-          <a class="nav-link " href="../pages/virtual-reality.html">
+          <a class="nav-link " href="pages/virtual-reality.html">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
             <i class="fa-solid fa-building-shield"></i>  
             <i class="ni ni-app text-info text-sm opacity-10"></i>
@@ -118,8 +123,21 @@
             <span class="nav-link-text ms-1">Feedback</span>
           </a>
         </li>
+
+
+        <li class="nav-item">
+          <a class="nav-link " href="users.php">
+            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+            <i class="fa-solid fa-building-shield"></i>  
+            <i class="ni ni-app text-info text-sm opacity-10"></i>
+            </div>
+            <span class="font-weight-bolder text-white mb-0">Users</span>
+          </a>
+        </li>
+
       </ul>
     </div>
+    
 
   </aside>
   <main class="main-content position-relative border-radius-lg ">
@@ -160,13 +178,11 @@
       </div>
     </nav>
     <!-- End Navbar -->
-    <header class="bg-gradient-dark">
-    <div class="page-header min-vh-90 opacity-5" style="background-image: url('../images/bg9.jpg');">
-
+ 
     <div class="container-fluid py-4">
       <div class="row mt-4">
         <div class="col-lg-7 mb-lg-0 mb-4">
-          <div class="card">
+          <div class="card ">
             <div class="card-header pb-0 p-3">
               <div class="d-flex justify-content-between">
                 <h6 class="mb-2">Sales by Country</h6>
@@ -179,7 +195,7 @@
                     <td class="w-30">
                       <div class="d-flex px-2 py-1 align-items-center">
                         <div>
-                          <img src="img/icons/flags/US.png" alt="Country flag">
+                          <images src="../images/icons/flags/US.png" alt="Country flag">
                         </div>
                         <div class="ms-4">
                           <p class="text-xs font-weight-bold mb-0">Country:</p>
@@ -210,7 +226,7 @@
                     <td class="w-30">
                       <div class="d-flex px-2 py-1 align-items-center">
                         <div>
-                          <img src="img/icons/flags/DE.png" alt="Country flag">
+                          <images src="../images/icons/flags/DE.png" alt="Country flag">
                         </div>
                         <div class="ms-4">
                           <p class="text-xs font-weight-bold mb-0">Country:</p>
@@ -241,7 +257,7 @@
                     <td class="w-30">
                       <div class="d-flex px-2 py-1 align-items-center">
                         <div>
-                          <img src="img/icons/flags/GB.png" alt="Country flag">
+                          <images src="../images/icons/flags/GB.png" alt="Country flag">
                         </div>
                         <div class="ms-4">
                           <p class="text-xs font-weight-bold mb-0">Country:</p>
@@ -272,7 +288,7 @@
                     <td class="w-30">
                       <div class="d-flex px-2 py-1 align-items-center">
                         <div>
-                          <img src="img/icons/flags/BR.png" alt="Country flag">
+                          <images src="../images/icons/flags/BR.png" alt="Country flag">
                         </div>
                         <div class="ms-4">
                           <p class="text-xs font-weight-bold mb-0">Country:</p>
@@ -307,7 +323,152 @@
 
       </div>
       </div>
-  </header>
+
+
+      <div class="container-fluid py-4">
+      <div class="row mt-4">
+        <div class="col-lg-7 mb-lg-0 mb-4">
+          <div class="card ">
+            <div class="card-header pb-0 p-3">
+              <div class="d-flex justify-content-between">
+                <h6 class="mb-2">Sales by Country</h6>
+              </div>
+            </div>
+            <div class="table-responsive">
+              <table class="table align-items-center ">
+                <tbody>
+                  <tr>
+                    <td class="w-30">
+                      <div class="d-flex px-2 py-1 align-items-center">
+                        <div>
+                          <images src="../images/icons/flags/US.png" alt="Country flag">
+                        </div>
+                        <div class="ms-4">
+                          <p class="text-xs font-weight-bold mb-0">Country:</p>
+                          <h6 class="text-sm mb-0">United States</h6>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Sales:</p>
+                        <h6 class="text-sm mb-0">2500</h6>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Value:</p>
+                        <h6 class="text-sm mb-0">$230,900</h6>
+                      </div>
+                    </td>
+                    <td class="align-middle text-sm">
+                      <div class="col text-center">
+                        <p class="text-xs font-weight-bold mb-0">Bounce:</p>
+                        <h6 class="text-sm mb-0">29.9%</h6>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="w-30">
+                      <div class="d-flex px-2 py-1 align-items-center">
+                        <div>
+                          <images src="../images/icons/flags/DE.png" alt="Country flag">
+                        </div>
+                        <div class="ms-4">
+                          <p class="text-xs font-weight-bold mb-0">Country:</p>
+                          <h6 class="text-sm mb-0">Germany</h6>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Sales:</p>
+                        <h6 class="text-sm mb-0">3.900</h6>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Value:</p>
+                        <h6 class="text-sm mb-0">$440,000</h6>
+                      </div>
+                    </td>
+                    <td class="align-middle text-sm">
+                      <div class="col text-center">
+                        <p class="text-xs font-weight-bold mb-0">Bounce:</p>
+                        <h6 class="text-sm mb-0">40.22%</h6>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="w-30">
+                      <div class="d-flex px-2 py-1 align-items-center">
+                        <div>
+                          <images src="../images/icons/flags/GB.png" alt="Country flag">
+                        </div>
+                        <div class="ms-4">
+                          <p class="text-xs font-weight-bold mb-0">Country:</p>
+                          <h6 class="text-sm mb-0">Great Britain</h6>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Sales:</p>
+                        <h6 class="text-sm mb-0">1.400</h6>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Value:</p>
+                        <h6 class="text-sm mb-0">$190,700</h6>
+                      </div>
+                    </td>
+                    <td class="align-middle text-sm">
+                      <div class="col text-center">
+                        <p class="text-xs font-weight-bold mb-0">Bounce:</p>
+                        <h6 class="text-sm mb-0">23.44%</h6>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="w-30">
+                      <div class="d-flex px-2 py-1 align-items-center">
+                        <div>
+                          <images src="../images/icons/flags/BR.png" alt="Country flag">
+                        </div>
+                        <div class="ms-4">
+                          <p class="text-xs font-weight-bold mb-0">Country:</p>
+                          <h6 class="text-sm mb-0">Brasil</h6>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Sales:</p>
+                        <h6 class="text-sm mb-0">562</h6>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Value:</p>
+                        <h6 class="text-sm mb-0">$143,960</h6>
+                      </div>
+                    </td>
+                    <td class="align-middle text-sm">
+                      <div class="col text-center">
+                        <p class="text-xs font-weight-bold mb-0">Bounce:</p>
+                        <h6 class="text-sm mb-0">32.14%</h6>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      </div>
 
       <footer class="footer pt-3  ">
         <div class="container-fluid">
@@ -331,12 +492,12 @@
    
   </main>
 
-  <!--   Core JS Files   -->
-  <script src="../js/core/popper.min.js" type="text/javascript"></script>
-  <script src="../js/core/bootstrap.min.js" type="text/javascript"></script>
-  <script src="../js/plugins/perfect-scrollbar.min.js"></script>
+  <!--   Core ../js Files   -->
+  <script src="../js/core/popper.min.../js" type="text/javascript"></script>
+  <script src="../js/core/bootstrap.min.../js" type="text/javascript"></script>
+  <script src="../js/plugins/perfect-scrollbar.min.../js"></script>
   <!-- Control Center for Material UI Kit: parallax effects, scripts for the example pages etc -->
-  <script src="../js/material-kit.min.js?v=3.0.4" type="text/javascript"></script>
+  <script src="../js/material-kit.min.../js?v=3.0.4" type="text/javascript"></script>
 </body>
 
 </html>
--- app/home.php
+++ app/home.php
@@ -34,9 +34,14 @@
 </head>
 
 <body class="g-sidenav-show bg-dark">
-  <div class="min-height-300 bg-dark position-absolute w-100"></div>
-  <aside class="sidenav bg-secondary navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
-    <div class="sidenav-header">
+  <div class="min-height-300 bg-dark position-absolute w-100"  
+  style="background-image: url('images/wallpaper.jpg'); background-repeat: no-repeat; background-size: cover; background-blend-mode: lighten;">
+    <div class="sidenav-header" ></div>
+
+  
+  <aside class="sidenav bg-secondary navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main" 
+  style="background-image: url('images/bg9.jpg'); background-repeat: no-repeat; background-size: cover; background-blend-mode: lighten;">
+    <div class="sidenav-header" >
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href="" target="_blank">
         <images src="images/logo.png" class="navbar-brand-images h-100" alt="main_logo">
@@ -160,9 +165,7 @@
       </div>
     </nav>
     <!-- End Navbar -->
-    <header class="bg-gradient-dark">
-    <div class="page-header min-vh-90 opacity-5" style="background-image: url('images/bg9.jpg');">
-
+ 
     <div class="container-fluid py-4">
       <div class="row mt-4">
         <div class="col-lg-7 mb-lg-0 mb-4">
@@ -307,7 +310,152 @@
 
       </div>
       </div>
-  </header>
+
+
+      <div class="container-fluid py-4">
+      <div class="row mt-4">
+        <div class="col-lg-7 mb-lg-0 mb-4">
+          <div class="card ">
+            <div class="card-header pb-0 p-3">
+              <div class="d-flex justify-content-between">
+                <h6 class="mb-2">Sales by Country</h6>
+              </div>
+            </div>
+            <div class="table-responsive">
+              <table class="table align-items-center ">
+                <tbody>
+                  <tr>
+                    <td class="w-30">
+                      <div class="d-flex px-2 py-1 align-items-center">
+                        <div>
+                          <images src="images/icons/flags/US.png" alt="Country flag">
+                        </div>
+                        <div class="ms-4">
+                          <p class="text-xs font-weight-bold mb-0">Country:</p>
+                          <h6 class="text-sm mb-0">United States</h6>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Sales:</p>
+                        <h6 class="text-sm mb-0">2500</h6>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Value:</p>
+                        <h6 class="text-sm mb-0">$230,900</h6>
+                      </div>
+                    </td>
+                    <td class="align-middle text-sm">
+                      <div class="col text-center">
+                        <p class="text-xs font-weight-bold mb-0">Bounce:</p>
+                        <h6 class="text-sm mb-0">29.9%</h6>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="w-30">
+                      <div class="d-flex px-2 py-1 align-items-center">
+                        <div>
+                          <images src="images/icons/flags/DE.png" alt="Country flag">
+                        </div>
+                        <div class="ms-4">
+                          <p class="text-xs font-weight-bold mb-0">Country:</p>
+                          <h6 class="text-sm mb-0">Germany</h6>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Sales:</p>
+                        <h6 class="text-sm mb-0">3.900</h6>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Value:</p>
+                        <h6 class="text-sm mb-0">$440,000</h6>
+                      </div>
+                    </td>
+                    <td class="align-middle text-sm">
+                      <div class="col text-center">
+                        <p class="text-xs font-weight-bold mb-0">Bounce:</p>
+                        <h6 class="text-sm mb-0">40.22%</h6>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="w-30">
+                      <div class="d-flex px-2 py-1 align-items-center">
+                        <div>
+                          <images src="images/icons/flags/GB.png" alt="Country flag">
+                        </div>
+                        <div class="ms-4">
+                          <p class="text-xs font-weight-bold mb-0">Country:</p>
+                          <h6 class="text-sm mb-0">Great Britain</h6>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Sales:</p>
+                        <h6 class="text-sm mb-0">1.400</h6>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Value:</p>
+                        <h6 class="text-sm mb-0">$190,700</h6>
+                      </div>
+                    </td>
+                    <td class="align-middle text-sm">
+                      <div class="col text-center">
+                        <p class="text-xs font-weight-bold mb-0">Bounce:</p>
+                        <h6 class="text-sm mb-0">23.44%</h6>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="w-30">
+                      <div class="d-flex px-2 py-1 align-items-center">
+                        <div>
+                          <images src="images/icons/flags/BR.png" alt="Country flag">
+                        </div>
+                        <div class="ms-4">
+                          <p class="text-xs font-weight-bold mb-0">Country:</p>
+                          <h6 class="text-sm mb-0">Brasil</h6>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Sales:</p>
+                        <h6 class="text-sm mb-0">562</h6>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="text-center">
+                        <p class="text-xs font-weight-bold mb-0">Value:</p>
+                        <h6 class="text-sm mb-0">$143,960</h6>
+                      </div>
+                    </td>
+                    <td class="align-middle text-sm">
+                      <div class="col text-center">
+                        <p class="text-xs font-weight-bold mb-0">Bounce:</p>
+                        <h6 class="text-sm mb-0">32.14%</h6>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      </div>
 
       <footer class="footer pt-3  ">
         <div class="container-fluid">
