22d6575b27be26b4b9b9ea1116eeec84fd8c0735
Alan Nato <alanwnato@gmail.com>
2024-01-10 03:49:00 +0300

    fix : minor fixes

--- app/dashboard/users.php
+++ app/dashboard/users.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+$connect = mysqli_connect("localhost", "root", "", "utumishi");
+if(!isset($_SESSION['sess_user'])){
+  header("Location:login.php");
+}
+else
+{
+?>
+
 <!--
 =========================================================
 * Argon Dashboard 2 - v2.0.4
@@ -35,29 +45,28 @@
 
 <body class="g-sidenav-show bg-dark">
   <div class="min-height-300 bg-dark position-absolute w-100"  
-  style="background-image: url('../images/wallpaper.jpg'); background-repeat: no-repeat; background-size: cover; background-blend-mode: lighten;">
+  style="background-image: url('../images/wallpaper3.jpg'); background-repeat: no-repeat; background-size: cover; background-blend-mode: lighten;">
     <div class="sidenav-header" ></div>
 
   
-  <aside class="sidenav bg-secondary navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main" 
-  style="background-image: url('../images/bg9.jpg'); background-repeat: no-repeat; background-size: cover; background-blend-mode: lighten;">
+  <aside class="sidenav bg-secondary navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
     <div class="sidenav-header" >
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href="" target="_blank">
         <images src="../images/logo.png" class="navbar-brand-images h-100" alt="main_logo">
-        <span class="font-weight-bolder text-white mb-0">Utumishi</span>
+        <span class="h5 text-white">Utumishi</span>
         <br>
         <br>
-        <h6 class="font-weight-bolder text-white mb-0">Logged In As : </h6>
+        <h6 class="h5 text-white ">Logged In As : </h6>
 
         <br>
         <br>
-        <h6 class="font-weight-bolder text-white mb-0">Online</h6>
+        <h6 class="h5 text-white">Online</h6>
 
         <br>
         <br>
 
-        <a href="logout.php" class="font-weight-bolder text-white mb-0">Sign Out</h6>
+        <a href="logout.php" class="h5 text-white">Sign Out</h6>
 
         <br>
         <br>
@@ -131,9 +140,10 @@
             <i class="fa-solid fa-building-shield"></i>  
             <i class="ni ni-app text-info text-sm opacity-10"></i>
             </div>
-            <span class="font-weight-bolder text-white mb-0">Users</span>
+            <span class="h5 text-white">Users</span>
           </a>
         </li>
+        
 
       </ul>
     </div>
@@ -148,7 +158,7 @@
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
             <li class="breadcrumb-item text-sm text-white active" aria-current="page">Dashboard</li>
           </ol>
-          <h6 class="font-weight-bolder text-white mb-0">Users</h6>
+          <h6 class="h5 text-white">Users</h6>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
@@ -179,7 +189,7 @@
     </nav>
     <!-- End Navbar -->
  
-        <div class="container-fluid py-4">
+        <!--div class="container-fluid py-4">
       <div class="row mt-4">
         <div class="col-lg-12 mb-lg-0 mb-4">
           <div class="card ">
@@ -467,146 +477,82 @@
         </div>
 
       </div>
-      </div>
+      </div-->
 
       <div class="container-fluid py-4">
       <div class="row mt-4">
         <div class="col-lg-12 mb-lg-0 mb-4">
           <div class="card ">
-            <div class="card-header pb-0 p-3">
-              <div class="d-flex justify-content-between">
-                <h6 class="mb-2">Users</h6>
+
+      
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+    
+              <div class="bg-gradient-secondary shadow-secondary border-radius-lg pt-4 pb-3">
+              <h6 class="text-white text-capitalize text-center ps-3">Users</h6>
+              <h6 class="text-white text-capitalize text-center ps-3">Products</h6>
+                <center><button type="button" class="btn btn-block bg-gradient-primary mb-3" data-bs-toggle="modal" data-bs-target="#modal-default">Add User</button></center>
+            </div>
+            </div>
+            <div class="card-body px-0 pb-2">
+              <div class="table-responsive p-0">
+                <table class="table align-items-center mb-0">
+                  <thead>
+                    <tr>
+                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">#</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Product Name</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Description</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Price</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Image</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Delete</th>
+                      <th class="text-secondary opacity-7"></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>
+                      <div class="d-flex flex-column justify-content-center">
+                        <?php 
+                    $query = "SELECT * FROM feedback ORDER BY id ASC";
+                    $result = mysqli_query($connect, $query);
+                    if(mysqli_num_rows($result) > 0)
+                    {
+                      while ($row = mysqli_fetch_array($result)) {
+                        ?>
+                    
+                      <tr>
+                        <td class="font-weight-bold mb-0 align-middle text-center"><?php echo $row["id"]; ?></td>
+                        <td class="font-weight-bold mb-0 align-middle text-center"><?php echo $row["fullname"]; ?></td>
+                        <td class="font-weight-bold mb-0 align-middle text-center"><?php echo $row["email"]; ?></td>
+                        <td class="font-weight-bold mb-0 align-middle text-center"><?php echo $row["sentsms"]; ?> </td>
+
+                        <td><input type="button" class="font-weight-bold mb-0 align-middle text-center" 
+                        onclick="deleteproduct(<?php echo $row['id']; ?>)" name="Deleteprod" value="Delete"></td>
+                      </tr>
+                    
+                      <?php
+                    }
+}
+                      ?>
+                    </tr>
+                  </tbody>
+                </table>
+                <script language="javascript">
+  function deleteproduct(delid)
+  {
+    if(confirm("Do you want to delete this record?")){
+      window.location.href='deleteproducts.php?del_id=' +delid+'';
+      return true;
+    }
+  }
+</script>
               </div>
             </div>
-            <div class="table-responsive">
-              <table class="table align-items-center ">
-                <tbody>
-                  <tr>
-                    <td class="w-30">
-                      <div class="d-flex px-2 py-1 align-items-center">
-                        <div>
-                          <images src="../images/icons/flags/US.png" alt="Country flag">
-                        </div>
-                        <div class="ms-4">
-                          <p class="text-xs font-weight-bold mb-0">Country:</p>
-                          <h6 class="text-sm mb-0">United States</h6>
-                        </div>
-                      </div>
-                    </td>
-                    <td>
-                      <div class="text-center">
-                        <p class="text-xs font-weight-bold mb-0">Sales:</p>
-                        <h6 class="text-sm mb-0">2500</h6>
-                      </div>
-                    </td>
-                    <td>
-                      <div class="text-center">
-                        <p class="text-xs font-weight-bold mb-0">Value:</p>
-                        <h6 class="text-sm mb-0">$230,900</h6>
-                      </div>
-                    </td>
-                    <td class="align-middle text-sm">
-                      <div class="col text-center">
-                        <p class="text-xs font-weight-bold mb-0">Bounce:</p>
-                        <h6 class="text-sm mb-0">29.9%</h6>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td class="w-30">
-                      <div class="d-flex px-2 py-1 align-items-center">
-                        <div>
-                          <images src="../images/icons/flags/DE.png" alt="Country flag">
-                        </div>
-                        <div class="ms-4">
-                          <p class="text-xs font-weight-bold mb-0">Country:</p>
-                          <h6 class="text-sm mb-0">Germany</h6>
-                        </div>
-                      </div>
-                    </td>
-                    <td>
-                      <div class="text-center">
-                        <p class="text-xs font-weight-bold mb-0">Sales:</p>
-                        <h6 class="text-sm mb-0">3.900</h6>
-                      </div>
-                    </td>
-                    <td>
-                      <div class="text-center">
-                        <p class="text-xs font-weight-bold mb-0">Value:</p>
-                        <h6 class="text-sm mb-0">$440,000</h6>
-                      </div>
-                    </td>
-                    <td class="align-middle text-sm">
-                      <div class="col text-center">
-                        <p class="text-xs font-weight-bold mb-0">Bounce:</p>
-                        <h6 class="text-sm mb-0">40.22%</h6>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td class="w-30">
-                      <div class="d-flex px-2 py-1 align-items-center">
-                        <div>
-                          <images src="../images/icons/flags/GB.png" alt="Country flag">
-                        </div>
-                        <div class="ms-4">
-                          <p class="text-xs font-weight-bold mb-0">Country:</p>
-                          <h6 class="text-sm mb-0">Great Britain</h6>
-                        </div>
-                      </div>
-                    </td>
-                    <td>
-                      <div class="text-center">
-                        <p class="text-xs font-weight-bold mb-0">Sales:</p>
-                        <h6 class="text-sm mb-0">1.400</h6>
-                      </div>
-                    </td>
-                    <td>
-                      <div class="text-center">
-                        <p class="text-xs font-weight-bold mb-0">Value:</p>
-                        <h6 class="text-sm mb-0">$190,700</h6>
-                      </div>
-                    </td>
-                    <td class="align-middle text-sm">
-                      <div class="col text-center">
-                        <p class="text-xs font-weight-bold mb-0">Bounce:</p>
-                        <h6 class="text-sm mb-0">23.44%</h6>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td class="w-30">
-                      <div class="d-flex px-2 py-1 align-items-center">
-                        <div>
-                          <images src="../images/icons/flags/BR.png" alt="Country flag">
-                        </div>
-                        <div class="ms-4">
-                          <p class="text-xs font-weight-bold mb-0">Country:</p>
-                          <h6 class="text-sm mb-0">Brasil</h6>
-                        </div>
-                      </div>
-                    </td>
-                    <td>
-                      <div class="text-center">
-                        <p class="text-xs font-weight-bold mb-0">Sales:</p>
-                        <h6 class="text-sm mb-0">562</h6>
-                      </div>
-                    </td>
-                    <td>
-                      <div class="text-center">
-                        <p class="text-xs font-weight-bold mb-0">Value:</p>
-                        <h6 class="text-sm mb-0">$143,960</h6>
-                      </div>
-                    </td>
-                    <td class="align-middle text-sm">
-                      <div class="col text-center">
-                        <p class="text-xs font-weight-bold mb-0">Bounce:</p>
-                        <h6 class="text-sm mb-0">32.14%</h6>
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
+          </div>
+        </div>
+      </div>
               </table>
+
+     
             </div>
           </div>
         </div>
@@ -645,3 +591,6 @@
 </body>
 
 </html>
+<?php
+}
+?>
